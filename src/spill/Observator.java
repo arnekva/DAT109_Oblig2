ce65451edecfea8fac8d62332d3fc4410cc07528
Arne <arnekva@gmail.com>
2019-02-15 14:01:16 +0100

    Added rounds
    Added support for playing several rounds with the same or new players

--- src/spill/Observator.java
+++ src/spill/Observator.java
@@ -22,43 +22,72 @@ public class Observator {
 	public static void main(String[] args) {
 		Terningspill spill = new Terningspill(1);
 		Scanner tast = new Scanner(System.in);
-		System.out.println("Hvor mange spillere skal spille: ");
-		int antall = tast.nextInt();
-		tast.nextLine();
+		int runde = 2;
+		boolean ferdig = false;
+		boolean nyeDeltakere = true;
+		while (!ferdig) {
+			if (nyeDeltakere) {
+				spill = new Terningspill(1);
 
-		for (int i = 1; i <= antall; i++) {
-			System.out.println("\nSkriv navn på spiller " + i + ": ");
-			spill.leggTilSpiller(tast.nextLine());
-		}
+				System.out.println("Hvor mange spillere skal spille: ");
+				int antall = tast.nextInt();
+				tast.nextLine();
 
-		spill.spill();
-		tast.close();
-
-		ArrayList<Spiller> spillere = spill.getSpillere();
-		
-		System.out.println("\n*** RESULTATER *** \n");
-		spillere.stream().forEach(x -> System.out.println(x.toString()));
-
-		Spiller vinner = spillere.stream().max(Comparator.comparing(Spiller::getVerdi))
-				.orElseThrow(NoSuchElementException::new);
-
-		List<Spiller> vinnere = spillere.stream().filter(x -> x.getVerdi() == vinner.getVerdi())
-				.collect(Collectors.toList());
-
-		if (vinnere.size() > 1) {
-			System.out.println("\nDet er flere vinnere! Gratulerer til: ");
-			for (int d = 0; d < vinnere.size(); d++) {
-				System.out.print(vinnere.get(d).getNavn());
-				if(d < vinnere.size()-2) {
-					System.out.print(", ");
-				} else if(d < vinnere.size()-1) {
-					System.out.print(" og ");
-				} else {
-					System.out.print("!");
+				for (int i = 1; i <= antall; i++) {
+					System.out.println("\nSkriv navn på spiller " + i + ": ");
+					spill.leggTilSpiller(tast.nextLine());
 				}
 			}
-		} else {
-			System.out.println("\nVinneren er " + vinner.getNavn() + "!");
+			spill.spill();
+
+			ArrayList<Spiller> spillere = spill.getSpillere();
+
+			System.out.println("\n*** RESULTATER *** \n");
+			spillere.stream().forEach(x -> System.out.println(x.toString()));
+
+			Spiller vinner = spillere.stream().max(Comparator.comparing(Spiller::getVerdi))
+					.orElseThrow(NoSuchElementException::new);
+
+			List<Spiller> vinnere = spillere.stream().filter(x -> x.getVerdi() == vinner.getVerdi())
+					.collect(Collectors.toList());
+
+			if (vinnere.size() > 1) {
+				System.out.println("\nDet er flere vinnere! Gratulerer til: ");
+				for (int d = 0; d < vinnere.size(); d++) {
+					System.out.print(vinnere.get(d).getNavn());
+					if (d < vinnere.size() - 2) {
+						System.out.print(", ");
+					} else if (d < vinnere.size() - 1) {
+						System.out.print(" og ");
+					} else {
+						System.out.print("!");
+					}
+				}
+			} else {
+				System.out.println("\nVinneren er " + vinner.getNavn() + "!");
+			}
+			System.out.println(
+					"\nØnsker du å spille en runde til? Skriv 'like' for å spille med samme deltakere eller 'nye' for å skrive inn nye deltakere. Trykk enter for å avslutte.");
+			String svar = tast.nextLine();
+
+			switch (svar) {
+			case ("like"):
+				System.out.println("\nSpiller runde " + runde + "!");
+				nyeDeltakere = false;
+				
+				break;
+			case ("nye"):
+				System.out.println("\nSpiller runde " + runde + "!");
+				nyeDeltakere = true;
+				
+				break;
+			default:
+				ferdig = true;
+				System.out.println("\nSpillet avsluttes!");
+				tast.close();
+				break;
+			}
+			runde++;
 		}
 	}
 
